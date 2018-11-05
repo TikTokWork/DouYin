a27f1cc565e7c4503b4acfd869d55cc6ad92d725
JacksonYANG <353904675@qq.com>
2018-11-05 21:11:50 +0800

    v1.1 完成网站前端的页面
    Signed-off-by: JacksonYANG <353904675@qq.com>

--- TikTokBot/templates/static/js/style.js
+++ TikTokBot/templates/static/js/style.js
@@ -5,8 +5,37 @@ $(document).ready(function () {
         $.css("background", "red");
     }).click(function () {
         $.css("background", "blue");
-        var data_douyin = {'user_id': $("#douyin-user-id").val()};
-        $.getJSON('/api', data_douyin, function (json_object) {
+        $.ajax({
+            url: "/api/douyin",
+            dataType: "json",
+            data: {'id': $("#douyin-user-id").val()},
+            type: "GET",
+            success: function (response) {
+                alert("请求成功");
+                var authorId = response["id"];
+                var authorDesc = response["author_desc"];
+                var nickname = response["nickname"];
+                var list = response["aweme_list"];
+                //获取作者的id，作者昵称，作者简介
+                $("#response-user-id").append(authorId);
+                $("#nickname").append(nickname);
+                $("#user-desc").append(authorDesc);
+
+                //新建新的节点获取作品的简介，作者的URL
+                list.forEach(function (object) {
+                    var treeH = document.createElement("tr");
+                    for (i in object){
+                        var treeNode = document.createElement("td");
+                        treeNode.innerText = i;
+                        treeH.appendChild(treeNode);
+                    }
+                    $("#list-body").appendChild(treeH);
+                })
+            },
+            error: function () {
+                alert("请求出错");
+                window.location.reload();
+            },
 
         })
 
@@ -16,11 +45,41 @@ $(document).ready(function () {
         $.css("background", "red");
     }).click(function () {
         $.css("background", "blue");
-        var data_tiktok = {'user_id': $("#douyin-user-id").val()};
-        $.getJSON('/api', data_tiktok, function (json_object) {
+        $.ajax({
+            url: "/api/tiktok",
+            dataType: "json",
+            data: {'id': $("#tiktok-user-id").val()},
+            type: "GET",
+            success: function (response) {
+                alert("请求成功");
+                var authorId = response["id"];
+                var authorDesc = response["author_desc"];
+                var nickname = response["nickname"];
+                var list = response["aweme_list"];
+                //获取作者的id，作者昵称，作者简介
+                $("#response-user-id").append(authorId);
+                $("#nickname").append(nickname);
+                $("#user-desc").append(authorDesc);
+
+                //新建新的节点获取作品的简介，作者的URL
+                list.forEach(function (object) {
+                    var treeH = document.createElement("tr");
+                    for (i in object){
+                        var treeNode = document.createElement("td");
+                        treeNode.innerText = i;
+                        treeH.appendChild(treeNode);
+                    }
+                    $("#list-body").appendChild(treeH);
+                })
+            },
+            error: function () {
+                alert("请求出错");
+                window.location.reload();
+            },
 
         })
 
 
     });
+
 });
